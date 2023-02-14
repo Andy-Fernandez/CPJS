9c76bfef1711e57b2c1e3e74f0ea9512e28883a2
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-02-14 11:34:27 -0400

    cambios varios en la calucladora

---
+++ calculadora/js/script.js
@@ -0,0 +1,12 @@
+const operation = document.querySelector('.operation');
+const result = document.querySelector('.result');
+9
+
+
+// result.innerText = "2"
+
+// operation.setAttribute('value', '5+5');
+function btnOnClickXd() {
+  result.innerText = "4"
+  console.log('AC button clicked');
+}
