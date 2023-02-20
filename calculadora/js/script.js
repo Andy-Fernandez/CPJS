1c7f266a047e3ab239a5f8d5743036ddce77dd03
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-02-19 23:39:30 -0400

    ,,,

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,12 +1,20 @@
 const operation = document.querySelector('.operation');
 const result = document.querySelector('.result');
-9
+const seven = document.querySelector('.seven');
+
 
 
 // result.innerText = "2"
+console.log(seven.innerText + "hi");
 
 // operation.setAttribute('value', '5+5');
 function btnOnClickXd() {
-  result.innerText = "4"
+  result.innerText = "0"
   console.log('AC button clicked');
 }
+function btnOnClickNumber(number) {
+  console.log('Number button clicked');
+  result.innerText += number;
+}
+
+
